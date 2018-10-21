5dbe683bd1a389e08d28aadd60bbf6f447a923f1
Christopher Thomas <sethosayher@gmail.com>
2018-10-20 22:23:53 -0400

    Added binShiftSearch

--- binSearch.java
+++ binSearch.java
@@ -1,4 +1,30 @@
+// log n operation - implementation by cpt2132
+import java.util.*;
+
 public class binSearch {
 
+	public static void main(String[] arg) {
+		int[] a = {0,2,5,134,900,35657};
+		System.out.println(bSearch(a, 900, 0, a.length - 1));
+	}
+
+	public static int  bSearch(int[] arr, int target, int lowerBound, int upperBound) {
 	
+		if(lowerBound > upperBound) {
+			return -1; // error condition
+		}
+
+		// find midpoint, compare to target
+		int mid = (lowerBound + upperBound) / 2;
+
+		if(arr[mid] == target) {
+			return mid;
+		} else if (target < arr[mid]) {
+			return bSearch(arr, target, lowerBound,  mid - 1);
+		} else {
+			return bSearch(arr, target, mid + 1, upperBound);
+		}
+
+
+	}	
 }
